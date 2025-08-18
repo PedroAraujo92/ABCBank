052bf982f5aecd467b50488200191a7409366f89
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-18 11:14:09 -0300

    Base entity

---
+++ Domain/Contracts/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public abstract class BaseEntity<TId> : IEntity<TId>
+        where TId : notnull
+    {
+        public TId Id { get; set; }
+        public BaseEntity()
+        {
+        }
+        public BaseEntity(TId id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Domain/Contracts/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts;
+
+internal interface IEntity<TId> : IEntity
+    where TId : notnull
+{
+    TId Id { get; set; }
+}
+
+internal interface IEntity
+{
+}
