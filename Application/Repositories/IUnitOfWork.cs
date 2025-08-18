d2cba9634e9346ce09c3a730f79f50e18637bcce
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-18 11:55:21 -0300

    Adding UnitOfWork interface

---
+++ Application/Repositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Contracts;
+
+namespace Application.Repositories;
+
+internal interface IUnitOfWork<TId> : IDisposable
+    where TId : notnull
+{
+    Task<int> CommitAsync();
+    Task<int> CommitAsync(CancellationToken cancellationToken);
+    IWriteRepositoryAsync<T, TId> GetWriteRepositoryFor<T>()
+        where T : BaseEntity<TId>;
+    IReadRepositoryAsync<T, TId> GetReadRepository<T>()
+        where T : BaseEntity<TId>;
+}
