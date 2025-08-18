42208f8b9b028f975ae2d470aa2f7eb330c3e212
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-18 12:15:42 -0300

    Db context - Ef Core

---
+++ Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AccountHolder> AccountHolders => Set<AccountHolder>();
+    public DbSet<Account> Accounts => Set<Account>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
+}
