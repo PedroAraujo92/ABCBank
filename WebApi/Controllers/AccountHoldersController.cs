537c63a13fa3398a2e3075a97cea677fba456537
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-22 11:09:15 -0300

    Account holder complete controller (CRUD)

--- WebApi/Controllers/AccountHoldersController.cs
+++ WebApi/Controllers/AccountHoldersController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.AccountHolders.Command;
+using Application.Features.AccountHolders.Queries;
 using Common.Requests;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,5 +18,49 @@ public class AccountHoldersController : BaseApiController
         }
 
         return BadRequest(result);
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateAccountHolderAsync([FromBody] UpdateAccountHolder updateAccountHolder)
+    {
+        var result = await Sender.Send(new UpdateAccountHolderCommand { UpdateAccountHolder = updateAccountHolder });
+        if (result.IsSuccessful)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> DeleteAccountHolderAsync(int id)
+    {
+        var result = await Sender.Send(new DeleteAccountHolderCommand { Id = id });
+        if (result.IsSuccessful)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+
+    [HttpGet("get/{id}")]
+    public async Task<IActionResult> GetAccountHolderAsync(int id)
+    {
+        var result = await Sender.Send(new GetAccountHolderByIdQuery { Id = id });
+        if (result.IsSuccessful)
+        {
+            return Ok(result);
+        }
+        return NotFound(result);
+    }
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAllAccountHoldersAsync()
+    {
+        var result = await Sender.Send(new GetAccountHoldersQuery ());
+        if (result.IsSuccessful)
+        {
+            return Ok(result);
+        }
+        return NotFound(result);
     }
 }
