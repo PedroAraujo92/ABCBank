f18bc2f31d81b0c0469bdce8bbf00df620300031
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-22 09:22:24 -0300

    Account holder request

---
+++ Common/Requests/AccountHolderRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Common.Requests;
+
+public record CreateAccountHolder(
+    string FirstName,
+    string LastName,
+    DateTime DateOfBirth,
+    string EmailAddress,
+    string ContactNumber);
+
+public record UpdateAccountHolder(
+    int Id,
+    string FirstName,
+    string LastName,
+    string EmailAddress,
+    string ContactNumber);
