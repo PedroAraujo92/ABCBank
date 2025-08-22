ce5bc6fb6163b94c490d89fb16f7e58afee35476
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-22 10:52:58 -0300

    Get account holders query

---
+++ Application/Features/AccountHolders/Queries/GetAccountHoldersQuery.cs
@@ -0,0 +1,25 @@
+﻿using Application.Repositories;
+using Common.Responses;
+using Common.Wrapper;
+using Domain;
+using Mapster;
+using MediatR;
+
+namespace Application.Features.AccountHolders.Queries;
+
+public class GetAccountHoldersQuery : IRequest<ResponseWrapper<List<AccountHolderResponse>>>
+{
+
+}
+
+public class GetAccountHoldersQueryHandler(IUnitOfWork<int> unitOfWork)
+    : IRequestHandler<GetAccountHoldersQuery, ResponseWrapper<List<AccountHolderResponse>>>
+{
+    private readonly IUnitOfWork<int> _unitOfWork = unitOfWork;
+    public async Task<ResponseWrapper<List<AccountHolderResponse>>> Handle(GetAccountHoldersQuery request, CancellationToken cancellationToken)
+    {
+        var accountHoldersInDb = await _unitOfWork.ReadRepositoryFor<AccountHolder>().GetAllAsync();
+        var response = accountHoldersInDb.Adapt<List<AccountHolderResponse>>();
+        return new ResponseWrapper<List<AccountHolderResponse>>().Success(response, "Account holders retrieved successfully.");
+    }
+}
