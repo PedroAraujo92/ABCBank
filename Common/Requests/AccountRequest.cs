cee2853db1c88c5e6eb549e6a5a41e769ee2ce52
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-25 10:13:32 -0300

    Adding transaction request

--- Common/Requests/AccountRequest.cs
+++ Common/Requests/AccountRequest.cs
@@ -7,3 +7,19 @@ public record CreateAccountRequest(
     decimal Balance,
     AccountType Type
 );
+
+//public record WithdrawalRequest(
+//    int AccountId,
+//    decimal Amount
+//);
+
+//public record DepositRequest(
+//    int AccountId,
+//    decimal Amount
+//);
+
+public record TransactionRequest(
+    int AccountId,
+    decimal Amount,
+    TransactionType Type
+);
