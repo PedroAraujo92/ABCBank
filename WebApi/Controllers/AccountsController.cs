41ffd0ea1ad0cda7a8a6dec1ec11f35c713f4821
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-25 09:08:42 -0300

    Add account endpoint

---
+++ WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Features.Accounts.Command;
+using Common.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController : BaseApiController
+{
+    [HttpPost]
+    public async Task<IActionResult> AddAccountAsync([FromBody]CreateAccountRequest createAccount)
+    {
+        var command = new CreateAccountCommand { CreateAccount = createAccount };
+        var response = await Sender.Send(command);
+
+        if (response.IsSuccessful)        
+        {
+            return Ok(response);
+        }
+
+        return BadRequest("Account creation failed");
+    }
+}
