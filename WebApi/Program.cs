7d344655e53615e5a8753c387c0efbc4543c8d52
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-25 09:15:46 -0300

    Enum as string in swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,7 @@
 
 using Infrastructure;
 using Application;
+using System.Text.Json.Serialization;
 
 namespace WebApi
 {
@@ -12,7 +13,11 @@ namespace WebApi
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
