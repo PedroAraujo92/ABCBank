0dda00eca10e5def12d98e5cc7f1053c4467ffed
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-25 09:25:50 -0300

    Get account by id query

---
+++ Application/Features/Accounts/Queries/GetAccountByIdQuery.cs
@@ -0,0 +1,31 @@
+﻿using Application.Repositories;
+using Common.Responses;
+using Common.Wrapper;
+using Domain;
+using Mapster;
+using MediatR;
+
+namespace Application.Features.Accounts.Queries;
+
+public class GetAccountByIdQuery : IRequest<ResponseWrapper<AccountResponse>>
+{
+    public int Id { get; set; }
+}
+
+public class GetAccountByIdQueryHandler : IRequestHandler<GetAccountByIdQuery, ResponseWrapper<AccountResponse>>
+{
+    private readonly IUnitOfWork<int> _unitOfWork;
+    public GetAccountByIdQueryHandler(IUnitOfWork<int> unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<ResponseWrapper<AccountResponse>> Handle(GetAccountByIdQuery request, CancellationToken cancellationToken)
+    {
+        var accountInDb = await _unitOfWork.ReadRepositoryFor<Account>().GetByIdAsync(request.Id);
+        if (accountInDb == null)
+        {
+            return new ResponseWrapper<AccountResponse>().Failed("Account not found");
+        }
+        return new ResponseWrapper<AccountResponse>().Success(accountInDb.Adapt<AccountResponse>(), "Account retrieved successfully");
+    }
+}
---
+++ Common/Responses/AccountResponse.cs
@@ -0,0 +1,12 @@
+﻿using Common.Enums;
+
+namespace Common.Responses;
+
+public class AccountResponse
+{
+    public string AccountNumber { get; set; }
+    public int AccountHolderId { get; set; }
+    public bool IsActive { get; set; }
+    public AccountType Type { get; set; }
+    public AccountHolderResponse AccountHolder { get; set; }
+}
