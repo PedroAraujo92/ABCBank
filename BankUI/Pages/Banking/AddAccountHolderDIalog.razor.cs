2005d8cc11f716b399cb1ea61acbe55d9b3ecfe7
Pedro Araujo <pe.araujo21@gmail.com>
2025-08-31 12:49:28 -0300

    Implementation of validation do create account holder

--- BankUI/Pages/Banking/AddAccountHolderDIalog.razor.cs
+++ BankUI/Pages/Banking/AddAccountHolderDIalog.razor.cs
@@ -1,4 +1,5 @@
-﻿using Common.Requests;
+﻿using BankUI.Pages.Banking.Validators;
+using Common.Requests;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
@@ -6,6 +7,8 @@ namespace BankUI.Pages.Banking;
 
 public partial class AddAccountHolderDialog
 {
+    private CreateAccountHolderValidator _validator = new();
+
     [Parameter]
     public CreateAccountHolder CreateAccountHolderRequest { get; set; } = new();
 
@@ -14,34 +17,29 @@ public partial class AddAccountHolderDialog
 
     MudForm _form = default;
 
-    public DateTime? DateOfBirth { get; set; }
-
-    private async Task SaveAsync()
+    private async Task SubmitAsync()
     {
         await _form.Validate();
         if (_form.IsValid)
         {
-            // Cast DateOfBirth to DateOnly and assign to CreateAccountHolderRequest
-            if (!DateOfBirth.HasValue)
-            {
-                _snackbar.Add("Date of Birth is required.", Severity.Error);
-                return;
-            }
-            CreateAccountHolderRequest.DateOfBirth = (DateTime)DateOfBirth;
-
-            var response = await _accountHolderService.AddAccountHolderAsync(CreateAccountHolderRequest);
-            if (!response.IsSuccessful)
-            {
-                foreach (var error in response.Messages)
-                {
-                    _snackbar.Add(error, Severity.Error);
-                }
-                return;
-            }
-
-            _snackbar.Add(response.Messages[0], Severity.Success);
-            MudDialog.Close(DialogResult.Ok(true));
+            await SaveAsync();
         }
+    }
+
+    private async Task SaveAsync()
+    {
+        var response = await _accountHolderService.AddAccountHolderAsync(CreateAccountHolderRequest);
+        if (!response.IsSuccessful)
+        {
+            foreach (var error in response.Messages)
+            {
+                _snackbar.Add(error, Severity.Error);
+            }
+            return;
+        }
+
+        _snackbar.Add(response.Messages[0], Severity.Success);
+        MudDialog.Close(DialogResult.Ok(true));
     }
 
     private void Cancel() => MudDialog.Cancel();
